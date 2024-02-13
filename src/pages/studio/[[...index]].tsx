ab700402a0df0c5c6b23cad3b9d4ec3e4b2462de
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2024-02-13 12:04:53 +0500

    code

--- src/pages/studio/[[...index]].tsx
+++ src/pages/studio/[[...index]].tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { NextStudio } from 'next-sanity/studio'
 import { metadata } from 'next-sanity/studio/metadata'
-import config from '../../../sanity.config'
+import config from "../../../sanity/sanity.config"
 
 export default function StudioPage() {
   return (
